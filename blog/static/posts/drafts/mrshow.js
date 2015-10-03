f6ff4d59acde9a47c6729d4be74b67e002f4c8c9
Alex Ramsdell <alexramsdell@gmail.com>
2015-10-03 15:28:45 -0700

    finished mr show post

--- blog/static/posts/drafts/mrshow.js
+++ blog/static/posts/drafts/mrshow.js
@@ -1,42 +1,40 @@
-/*var Corporation = function() {
-
-  this.employees = {
-    employee1: {
-      name:'Store Clerk',
-      boss: this.employees.employee2,
-      execPower: false,
-    },
-    employee2: {
-      name:'Store Manager',
-      boss: this.employees.employee3,
-      execPower: false,
-    },
-    employee3: {
-      name:'Regional Manager',
-      boss: this.employees.employee4,
-      execPower: false,
-    },
-    employee4: {
-      name: 'Corporate Executive',
-      boss: this.employees.employee5,
-      execPower: false,
-    },
-    employee5: {
-      name: 'President of the United States',
-      boss: null,
-      execPower: true,
-      changeForADollarPossible: function() {
-        return 'no';
-      }
+var employee5 = {
+  name: 'President of the United States',
+  boss: null,
+  authorize: function() {
+    return 'no';
+  }
+};
+var employee4 = {
+  name: 'Corporate Executive',
+  boss: employee5,
+};
+var employee3 = {
+  name:'Regional Manager',
+  boss: employee4,
+};
+var employee2 = {
+  name:'Store Manager',
+  boss: employee3,
+};
+var employee1 = {
+  name:'Store Clerk',
+  boss: employee2,
+};
+var customer = {
+  name: 'A. Guy',
+  say: function(phrase) { console.log(phrase)},
+  getChangeForADollar: function(employee) {
+    console.log('Asking the %s ...', employee.name);
+    if (employee.authorize) {
+      var answer = employee.authorize();
+      return 'the answer is ' + answer +'.'; 
     }
- };
-};
-*/
-
-
-var getAnswer = function(employee) {
-  if (employee.changeForADollarPossible) return employee.changeForADollarPossible(); 
-  else return getAnswer(employee.boss);
+    else {
+      return customer.getChangeForADollar(employee.boss);
+    }
+  }
 };
 
-console.log(new Corporation());
+customer.say('Could I have change for a dollar?');
+console.log(customer.getChangeForADollar(employee1));
--- blog/static/posts/drafts/recursion.py
+++ blog/static/posts/drafts/recursion.py
@@ -1,52 +1,78 @@
-CHANGE FOR A DOLLAR
+## Mr. Show, 'Change For a Dollar' and Recursion
 
-While the Mr. Show sketch 'Change for a Dollar' seems to be a joke on the fate of value in the hands of pure beaurocracy, it also demonstrates the winding and unwinding of function calls in a recursive operation quite nicely.
+If you're familiar with Mr. Show, you probably know that a technique common to some of the funniest (and worst) sketches is the assiduous repetition of a single idea. This technique is really effective in 'The Story of Everest' and 'The Audition', where the repetition takes on additional value over time.
 
-If you've seen at least a few of Mr. Show sketches, then you know that a large part of the comedy depends on assiduous repetition of a narrow concept.  However, 'Change for a Dollar' goes beyond that in using the same structure  
+The sketch 'Change for a Dollar' uses this tight repetition to play a good joke on the fate of value in the hands of pure beaurocracy. Oddly, it also quite nicely demonstrates the winding and unwinding of function calls in a recursive function.
 
-The gist of the sketch is that a man asks a grocery store clerk to exchange a dollar for four quarters. Instead of just saying yes or no, the clerk has to ask his boss for permission, and it turns out his boss must in turn ask his boss for permission, and so on until the original request reaches an employee with the credentials to say yes or no. At that point, the answer propagates back through each boss to his underling, until the answer reaches the context of the original requester where the employee can give the requester the answer.
+Recursion? Mr. Show?
 
-Each scene in the sketch effectively operates as the execution context for a new function placed on top of the call stack. In each new scene, the boss from the previous scene takes on the roll of the employee seeking an answer from his boss.  
+Yes, recursion! Yes, Mr. Show!
 
-It's time for some code. Here is a data structure that could represent the chain of command in this haplessly bureaucratic corporation (notice that employee5 is the only one who has executive power, meaning only he can sign off on the 'change for a dollar' request): 
+The gist of the sketch is that a man asks a convenience store clerk to exchange a dollar for four quarters. Instead of just saying yes or no, the clerk has to ask his boss for permission, and it turns out his boss must in turn ask *his* boss for permission, and so on, until the original request reaches an employee with the credentials to say yes or no. At that point, the answer propagates back through each boss to the requesting underling. When that underline is the store clerk, the customer receives the answer: 'no'.
+ 
+You can think of each scene as the 'execution context' of a new function placed on top of the call stack created by the request to make change. For every scene, the boss from the previous scene assumes the roll of the employee seeking an answer from his boss. If the employee cannot authorize the request, he must call the same 'procedure' on his boss.      
 
-var corporation = {
+Below, I've created a basic model to represent the chain of command in this haplessly bureaucratic corporation (notice that employee5 is the only one who has the `authorize` method, meaning only he can sign off on the 'change for a dollar' request): 
 
-  emp1: {
-    name:'Store Clerk',
-    boss: this.emp2,
-    execPower: false,
-  },
-  emp2: {
-    name:'Store Manager',
-    boss: this.emp3,
-    execPower: false,
-  },
-  emp3: {
-    name:'Regional Manager',
-    boss: this.emp4,
-    execPower: false,
-  },
-  emp4: {
-    name: 'Corporate Executive',
-    boss: this.emp5,
-    execPower: false,
-  },
-  emp5: {
-    name: 'President of the United States',
-    boss: null,
-    execPower: true,
-    changeForADollarPossible: function() {
-      return 'no';
-      // i'm sure Mr. Show would have this function separate from the actual act of doing the exchange
-      // and i bet the exchange function would require another entirely separate recursive operation
-    }
-  },
+````javascript
 
+var employee5 = {
+  name: 'President of the United States',
+  boss: null,
+  authorize: function() {
+    return 'no';
+  }
+};
+var employee4 = {
+  name: 'Corporate Executive',
+  boss: employee5,
+};
+var employee3 = {
+  name:'Regional Manager',
+  boss: employee4,
+};
+var employee2 = {
+  name:'Store Manager',
+  boss: employee3,
+};
+var employee1 = {
+  name:'Store Clerk',
+  boss: employee2,
 };
 
+````
 
+The five employee objects make up a singly-linked-list where each employee save the last has a reference to his boss, who has a reference to his boss, etc. We can use this structure to recurse through the list until an employee with an `authorize` method is found.
 
+````javascript
+var customer = {
+  name: 'customer',
+  say: function(phrase) { console.log(phrase)},
+  getChangeForADollar: function(employee) {
+    console.log('Asking the %s ...', employee.name);
+    if (employee.authorize) {
+      var answer = employee.authorize();
+      return 'the answer is ' + answer +'.'; 
+    }
+    else {
+      return this.getChangeForADollar(employee.boss);
+    }
+  }
+};
+````
 
-function changeForDollar?(employee, boss) {
-}
+When the `authorize` method is found on an employee, the results of that method call are returned. This is our base case, and causes each function on the call stack to resolve its return value until the original caller is reached. 
+
+````
+customer.say('I need change for a dollar.');
+console.log(customer.getChangeForADollar(employee1));
+````
+
+Why is this not iterative?
+
+Just in case you were wondering why I'm making a big deal about this being a good example of recursion instead of iteration using a while loop, there are two reasons:
+
+  1. The context for each authorization check is a totally new location relative to the specific employee and boss involved. If the sketch demonstrated iteration (like 'Story of Everest' and 'The Audition' mentioned above), the context of the employee traversal process would be the `customer.getChangeForADollar` method throughout, aka, the whole sketch would have to take place in the convenience store. 
+  2. The sketch demonstrates an explicit 'unwinding' phase where the ultimate answer is relayed from boss to underling in exactly the reverse order of the recursive calls. 
+
+Note: the pronouns in this post are all male. The reason is simply that the relevant roles in the sketch are played by David Cross and Bob Odenkirk, who in fact play all of the employees and bosses.
