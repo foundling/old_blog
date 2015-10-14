6410e64da7ce8d906002318d27ecab819f55164e
Alex Ramsdell <alexramsdell@gmail.com>
2015-10-14 10:46:09 -0700

    views revised to reflect new organization

--- blog/db_ops.py
+++ blog/db_ops.py
@@ -1,4 +1,11 @@
 def n_most_recent(db,n):
-    ''' n most recent published articles ''' 
-    return db.posts.find({'published':True}).sort('date_published')[:n]
+    ''' n_most_recent - gets n most recent published articles if n != 0. Else, gets all articles.''' 
+    if n == 0:
+      return db.posts.find({'published':True}).sort('date_published')[:]
+    else: 
+      return db.posts.find({'published':True}).sort('date_published')[:n]
 
+def findById(id):
+  return db.posts.find({'_id',ObjectId(id)})
+
+      
