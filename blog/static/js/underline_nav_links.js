2e322c31a2e6708d4a3b4a89442a69da9edb0ece
Alex Ramsdell <alexramsdell@gmail.com>
2015-10-29 17:44:49 -0700

    window location now maps to an id that highlights the corresponding nav element to give it an underline

---
+++ blog/static/js/underline_nav_links.js
@@ -0,0 +1,32 @@
+/*
+ * set of links.
+ * each window location maps to a link element, applies an id 
+ *
+ */
+
+console.log('pathname!: ', window.location.pathname);
+
+var routes = {
+  '/' :         'blog',
+  '/index.html' :    'blog',
+  '/index' :    'blog',
+  '/projects.html':  'projects',
+  '/projects':  'projects',
+  '/guides.html' :   'guides',
+  '/guides' :   'guides',
+  '/fun.html' :      'fun',
+  '/fun' :      'fun',
+  '/about.html' :    'about-me',
+  '/about' :    'about-me'
+};
+
+// get current location path
+var currentWindowLocation = window.location.pathname;
+
+// look it up and get the corresponding id
+var targetElementId = routes[currentWindowLocation];
+
+// target the element with that id
+var targetElement = document.getElementById(targetElementId);
+
+targetElement.className += ' current-page-highlight';
