897b857b443f95347a5f7f3d0572588a2931f967
Alex Ramsdell <alexramsdell@gmail.com>
2015-10-30 14:28:23 -0700

    to top arrow header toggle working in pure js

--- blog/static/js/media_toggle.js
+++ blog/static/js/media_toggle.js
@@ -1,1 +1,26 @@
+// distance from top of document
+// window distance from top
+// state change occurs when window offset from top of doc is greater than the element's distance from top, which is fixed 
 var socialMedia = document.getElementById('social-media');
+var headerHeight = 90;
+var pastHeader = false;
+var arrowEl = document.getElementById('to-top');
+
+
+
+
+var checkWindowOffsetFromTop = function(event) {
+  var windowOffsetY = window.pageYOffset;
+  if (windowOffsetY > headerHeight) {
+    pastHeader = true;
+    console.log('past header');  
+    arrowEl.style.display ='block';
+  }
+  else {
+    console.log('not past header');  
+    pastHeader = false;
+    arrowEl.style.display ='none';
+  }
+};
+
+window.onscroll = checkWindowOffsetFromTop;
