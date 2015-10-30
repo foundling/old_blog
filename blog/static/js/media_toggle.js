cfcfb8794fe73c7d860b72028ceca267f080edfd
Alex Ramsdell <alexramsdell@gmail.com>
2015-10-30 13:36:19 -0700

    header is looking good now

--- blog/static/js/media_toggle.js
+++ blog/static/js/media_toggle.js
@@ -1,13 +1,1 @@
-var mediaUpClass = 'media-up';
-var mediaDownClass = 'media-down';
-var up = true;
-var cn;
-
 var socialMedia = document.getElementById('social-media');
-socialMedia.onclick = function(e) {
-    up = !up;
-    if (!up) e.target.className = e.target.className.replace(mediaUpClass,mediaDownClass);
-    if (up) e.target.className = e.target.className.replace(mediaDownClass,mediaUpClass);
-    console.log(e.target.className);
-    console.log(up);
-};
