6937aa201a0b35474b74aa85d2403a8778b957a9
Alex Ramsdell <alexramsdell@gmail.com>
2015-11-12 10:39:32 -0800

    update script comments

--- push.sh
+++ push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Push all non-database changes to production server
+
 echo "cd'ing to /data/web"
 cd /data/web
 
@@ -10,8 +12,8 @@ find  /data/web/blog -name '*.pyc' -delete
 if [ "$1" == "-n" ]
 then
   echo 'mock syncing changes with the server ... (you passed the -n flag)'
-  rsync -auvnP --exclude={pushcontent.sh,rsync_exclude.txt,*push.sh,*.pyc,.sass-cache,*.scss,.gitignore,db,.git,.etc,env,freeze.py,docs} /data/web/blog/ alexr@$box:/home/alexr/alexramsdell.com/ 
+  rsync -auvnP --exclude={pushcontent.sh,db_exports,rsync_exclude.txt,*push.sh,*.pyc,.sass-cache,*.scss,.gitignore,db,.git,.etc,env,freeze.py,docs} /data/web/blog/ alexr@$box:/home/alexr/alexramsdell.com/ 
 else
-  rsync -auvP --exclude={pushcontent.sh,rsync_exclude.txt,*push.sh,*.pyc,.sass-cache,*.scss,.gitignore,db,.git,.etc,env,freeze.py,docs} /data/web/blog/ alexr@$box:/home/alexr/alexramsdell.com/ 
+  rsync -auvP --exclude={pushcontent.sh,db_exports,rsync_exclude.txt,*push.sh,*.pyc,.sass-cache,*.scss,.gitignore,db,.git,.etc,env,freeze.py,docs} /data/web/blog/ alexr@$box:/home/alexr/alexramsdell.com/ 
 fi
 
--- pushcontent.sh
+++ pushcontent.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+# push only database changes to server
+# export current, send to the server, drop those tables, import from my newly exported json array files
+
+if [ "$1" == "-n" ]
+then
+  echo 'testing changes, you passed -n flag'
+  rsync -auvnP db_exports/{posts,static,projects} alexr@$box:/home/alexr/alexramsdell.com/db_exports
+else
+  echo 'syncing local mongo databses with production server'
+rsync -auvP db_exports/{posts,static,projects} alexr@$box:/home/alexr/alexramsdell.com/db_exports
 ssh alexr@$box "cd /home/alexr/alexramsdell.com/db_exports && sh update_db.sh"
+fi
