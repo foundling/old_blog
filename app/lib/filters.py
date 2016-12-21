564342eb8ac69191f85ff1d6d8dc2d493b9adaae
Alex Ramsdell <alexramsdell@gmail.com>
2016-12-21 14:51:38 -0800

    date filters now return an empty string if no datetime object is supplied to prevent crash.

--- app/lib/filters.py
+++ app/lib/filters.py
@@ -4,10 +4,10 @@ def human_readable_date(datetime_object):
 
     ''' Takes an arrow.now().datetime object and returns a .humanize() version.'''
 
-    return arrow.Arrow.fromdatetime(datetime_object).humanize()
+    return arrow.Arrow.fromdatetime(datetime_object).humanize() if datetime_object else ''
 
 def clean_date(datetime_object):
 
     ''' Takes an arrow.now().datetime object and returns a .formatted version. '''
 
-    return arrow.Arrow.fromdatetime(datetime_object).format('MM-DD-YYYY')
+    return arrow.Arrow.fromdatetime(datetime_object).format('MM-DD-YYYY') if datetime_object else ''
