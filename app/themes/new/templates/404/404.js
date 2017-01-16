e1b724870f67b8f6ba677b8b029d6b1b03675e99
Alex Ramsdell <alexramsdell@gmail.com>
2017-01-15 18:08:10 -0800

    use two images for left and right directions. jump physics getting there.

--- app/themes/new/templates/404/404.js
+++ app/themes/new/templates/404/404.js
@@ -1,92 +1,125 @@
 const log = console.log;
-const gravity = 9.8;
 
-function init() {
+function run() {
 
     log('Initializing Calvin ...');
 
     const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    const imgs = [ new Image(), new Image() ];
+    let imagesLoaded = 0;
+
     canvas.width = 600;
     canvas.height = 600;
-    const ctx = canvas.getContext('2d');
-    const img = new Image();
-    img.src = 'http://pleated-jeans.com/wp-content/uploads/2011/02/calvin-and-hobbes-nes-sprite1.png'; //'calvin.png';
-    img.addEventListener('load', function() {
+    imgs[0].src = 'calvin-left.png';
+    imgs[1].src = 'calvin-right.png';
 
-        console.log('image loaded');
-        const calvin = new Player(img, { x: 100, y: 150 });
-        ctx.drawImage(calvin.img, 10, 0); 
-        console.log(img);
-        //loop(calvin, canvas, ctx);
+    imgs.forEach(function(img) {
+
+        img.addEventListener('load', function() {
+
+            if ( ++imagesLoaded < 2 ) return;
+
+            const calvin = new Player(imgs, ctx, {x: 100, y: 150}, 100);
+            calvin.init();
+
+        });
 
     });
-
 }
 
-function loop(player, canvas, ctx) {
+const Player = function(imgs, ctx, coordinates, mass) {
 
-    let intervalHandle = setInterval(function() {
+    if (!imgs || !imgs.length) throw new Error('Player object must be instantiated with an image posessing a valid .src property.');
+    if (!ctx || !ctx.canvas) throw new Error('Player object must be instantiated with a canvas object reference');
+    if (!(coordinates.x && coordinates.y)) throw new Error('Player object must be instantiated with x and y coordinates.');
 
-        player.update();
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(player.img, player.x, player.y); 
-
-    }, 1000/60);
-
-}
-
-
-const Player = function(img, coordinates) {
-
-    if (!(coordinates.x && coordinates.y)) {
-        throw new Error('Player object must be instantiated with x and y coordinates.');
-    }
-
-    const attrs = {
-        weight: 100,
-        coordinates: coordinates
+    let imageDirection = 'right';
+    let images = {
+        left: imgs[0],
+        right: imgs[1]
     };
+    let img = images[imageDirection];
+
+    let velocity = {
+        x: 0, 
+        y: -20,
+    };
+    let gravity = 0.5;
 
     const moves = [
-
         function jump(e) {
             if (e.key === 'ArrowUp') {
-                console.log(e.key);
+                animate();
+                console.log(velocity)
+                console.log(coordinates)
             }
         },
         function moveLeft(e) {
             if (e.key === 'ArrowLeft') {
+                updateDirection('left');
                 console.log(e.key);
             }
         },
         function moveRight(e) {
             if (e.key === 'ArrowRight') {
+                updateDirection('right');
                 console.log(e.key);
             }
         }
     ];
 
-    function update(x,y) {
-        // coordinates.x += x;
-        // coordinates.y += y;
-    };
-
     moves.forEach(move => document.addEventListener('keydown', move));
 
+    function redraw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, coordinates.x, coordinates.y); 
+    }
+    function update() {
+        velocity.y += gravity;
+        coordinates.y += velocity.y;
+        coordinates.x += velocity.x;  
+    }
+    function animate() {
+        let intervalHandle = setInterval(function() {
+            console.log('loop');
+
+            update()
+            redraw();
+
+            if (img.height + coordinates.y >= canvas.height) {
+                clearInterval(intervalHandle);
+            } 
+        }, 1000/60); 
+
+    }
+    function updateDirection(key) {
+        if (imageDirection === 'right' && key === 'left') {
+
+            imageDirection = 'left';
+            img = images[imageDirection];
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(img, coordinates.x, coordinates.y);
+
+        } else if (imageDirection === 'left' && key === 'right') {
+
+            imageDirection = 'right';
+            img = images[imageDirection];
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(img, coordinates.x, coordinates.y);
+
+        }
+
+    }
+    function init() {
+        ctx.drawImage(img, coordinates.x, coordinates.y); 
+    }
+
     return {
-        img,
-        coordinates,
+        init,
         update
     };
 
-    // reverse image on y axis
-    // jump up, left or right
-    // move left or right 
-
 };
 
-// redraw function to update canvas
-// keypress bindings
-//
-init();
+run();
